6945486656526808d1da3dbdd14fa355777329de
Taekjin Jang <xorwls1500@naver.com>
2023-03-17 19:14:16 +0900

    게시글 준비

--- front/src/pages/Board.jsx
+++ front/src/pages/Board.jsx
@@ -1,11 +1,21 @@
-import React from "react";
-// import { Route } from "react-router-dom";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import { TextArea } from 'react-bootstrap';
 
 function Board() {
   return (
     <>
       <div>
         <h1>홈</h1>
+        <label>Title: </label>
+        <input type="text" name="title" />
+        <hr></hr>
+        <label>작성자: </label>
+        <input type="text" name="title" />
+        <hr></hr>
+        <label>내용: </label>
+        <input type="text" name="title" />
+        <hr></hr>
       </div>
     </>
   );

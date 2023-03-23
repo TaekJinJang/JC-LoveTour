f92fa6910272ae0e4254e244885bfcb3d8f58b1e
Taekjin Jang <xorwls1500@naver.com>
2023-03-23 12:02:38 +0900

    이미지 클릯시 캐러셀 구현

--- front/src/components/Board/galleryBoardList.jsx
+++ front/src/components/Board/galleryBoardList.jsx
@@ -3,43 +3,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ImagesOnZoom from './imagesOnZoom';
-import { Modal } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
 
 function galleryBoardList({ post }) {
   const [showImagesZoom, setShowImagesZoom] = useState(false);
-  const onZoom = useCallback(() => {
-    setShowImagesZoom(true);
-    return (
-      <>
-        <Modal
-          show={showImagesZoom}
-          onHide={() => setShowCarousel(false)}
-          size="lg"
-          centered
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Image Carousel</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <ImagesOnZoom />
-          </Modal.Body>
-        </Modal>
-      </>
-    );
-  }, []);
+  const handleClose = () => setShowImagesZoom(false);
+
   return (
     <>
+      {/* 이미지 캐러셀 */}
+      <Modal show={showImagesZoom} onHide={handleClose} size="lg" centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Image Carousel</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ImagesOnZoom />
+        </Modal.Body>
+      </Modal>
+
       <Card style={{ width: '18rem' }}>
         <Card.Img
           variant="top"
           src={post.Images[0].src}
           alt={post.Images[0].src}
-          onClick={() => setShowImagesZoom(true)}
         />
         <Card.Body>
           <Card.Title>{post.title}</Card.Title>
           <Card.Text>{post.content}</Card.Text>
-          <Button variant="primary" onClick={onZoom}>
+          <Button variant="primary" onClick={() => setShowImagesZoom(true)}>
             더 보기
           </Button>
         </Card.Body>
--- front/src/components/Board/imagesOnZoom.jsx
+++ front/src/components/Board/imagesOnZoom.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Carousel from 'react-bootstrap/Carousel';
+import styled from 'styled-components';
 
 function imagesOnZoom() {
+  const Overlay = styled.div`
+    position: fixed;
+    z-index: 5000;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  `;
+  // const SlickWrapper = styled(Carousel.Item)`
+  //   height: calc(100% - 70px);
+  //   background: #090909;
+  // `;
+  const ResizeImg = styled.img`
+    height: '400px';
+    width: '300px';
+  `;
   return (
     <Carousel fade>
       <Carousel.Item>
@@ -10,6 +27,7 @@ function imagesOnZoom() {
           className="d-block w-100"
           src="https://gimg.gilbut.co.kr/book/BN001998/rn_view_BN001998.jpg"
           alt="First slide"
+          style={{ width: '400px', height: '700px' }}
         />
         <Carousel.Caption>
           <h3>First slide label</h3>
@@ -21,6 +39,7 @@ function imagesOnZoom() {
           className="d-block w-100"
           src="https://gimg.gilbut.co.kr/book/BN001998/rn_view_BN001998.jpg"
           alt="Second slide"
+          style={{ width: '400px', height: '700px' }}
         />
 
         <Carousel.Caption>

6606bcab026d9c0b9a8349e5a7f57c0f96e7715b
Taekjin Jang <xorwls1500@naver.com>
2023-05-08 10:37:24 +0900

    ui mergeing

--- front/src/components/Board/announceBoardView.jsx
+++ front/src/components/Board/announceBoardView.jsx
@@ -9,7 +9,22 @@ import styled from 'styled-components';
 import Pagination from 'react-js-pagination';
 import '../UI/paging.css';
 import '../UI/boardUI.css';
-import { Container, Nav, Navbar, NavDropdown, Button } from 'react-bootstrap';
+// 사이드바 라이브러리 추가
+import {
+  Container,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Button,
+  Row,
+  Col,
+  ButtonGroup,
+  Card,
+  Stack,
+  Form,
+  Badge,
+} from 'react-bootstrap';
+// 테이블 라이브러리 추가
 import { LOAD_POSTS_REQUEST } from '../../reducers/post';
 
 const Nomal_div = styled.div`
@@ -302,126 +317,182 @@ function announceBoardView() {
 
   return (
     <>
-      <NavWrapper>
-        <ul className="nav_var">
-          <li id="main_logo">
-            <a href="#">제천 러브투어</a>
-          </li>
-          <li className="dropdown">
-            <a href="#">알림마당 &#9662;</a>
-            <ul className="dropdown-menu">
-              <li>
-                <a className="sub_a" href="#">
-                  서브메뉴1
-                </a>
-              </li>
-              <li>
-                <a className="sub_a" href="#">
-                  서브메뉴2
-                </a>
-              </li>
-              <li>
-                <a className="sub_a" href="#">
-                  서브메뉴3
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li className="dropdown">
-            <a href="#">공지사항 &#9662;</a>
-            <ul className="dropdown-menu">
-              <li>
-                <a className="sub_a" href="#">
-                  서브메뉴1
-                </a>
-              </li>
-              <li>
-                <a className="sub_a" href="#">
-                  서브메뉴2
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </NavWrapper>
-      <Nomal_div>
-        <Side_title>
-          <Side_title_text>알림</Side_title_text>
-          <Side_title_text>마당</Side_title_text>
-        </Side_title>
-        <Board>
-          <h1>공지사항</h1>
-          <hr></hr>
-          <Search_bar>
-            {/* {admin && ( */}
-            <Link to="/board/announce/add">
-              <Button>글쓰기</Button>
-            </Link>
-            {/* )} */}
-            <Search_button onClick={goToSearch}>검색</Search_button>
-            <Search_input
-              type="text"
-              value={searchInput}
-              onChange={onChangeSearchInput}
-            />
-
-            <Sel_key name="sch_key">
-              <option value="@">글제목</option>
-              <option value="@">등록일자</option>
-              <option value="@">작성자</option>
-            </Sel_key>
-          </Search_bar>
-        </Board>
-        <Lm_list>
-          <Lm_list_item>
-            <Lm_list_item_link href="@">공지사항</Lm_list_item_link>
-          </Lm_list_item>
-          <Lm_list_item>
-            <Lm_list_item_link href="@">자주하는 질문</Lm_list_item_link>
-          </Lm_list_item>
-          <Lm_list_item>
-            <Lm_list_item_link href="@">1:1 고객센터</Lm_list_item_link>
-          </Lm_list_item>
-        </Lm_list>
-        <Table_form>
-          <Table>
-            <thead>
-              <tr>
-                <Th scope="col" width="10%">
-                  번호
-                </Th>
-                <Th scope="col" width="40%">
-                  제목
-                </Th>
-                <Th scope="col" width="15%">
-                  작성자
-                </Th>
-                <Th scope="col" width="20%">
-                  작성일
-                </Th>
-                <Th scope="col" width="10%">
-                  조회수
-                </Th>
-              </tr>
-            </thead>
-            <tbody>
-              {mainPosts.map((post, index) => (
-                <AnnounceBoardList key={post.id} post={post} page={page} />
-              ))}
-            </tbody>
-          </Table>
-
-          <Pagination
-            activePage={page}
-            itemsCountPerPage={10}
-            totalItemsCount={450}
-            pageRangeDisplayed={5}
-            prevPageText={'‹'}
-            nextPageText={'›'}
-            onChange={handlePageChange}
-          />
-        </Table_form>
-      </Nomal_div>
+      <Container>
+        <Row className="w-100% p-0">
+          {/* 상단 네비바 수정 부분 */}
+          <Navbar bg="success" expand="lg">
+            <Container>
+              <Navbar.Brand href="#home">
+                <h4>홈</h4>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav>
+                  <NavDropdown as="h5" title="알림마당" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">
+                      Action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">
+                      Something
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.4">
+                      Separated link
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown as="h5" title="공지사항" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">
+                      Action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">
+                      Something
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.4">
+                      Separated link
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </Row>
+        {/* 사이드바 받은 파일 */}
+        {/* // 2번 그리드 여기 아래의 코드가 바로 사이드바 코드 */}
+        <Row className="mt-3 ps-1">
+          <Col md={3} className="d-grid gap-2 ms" style={{ height: '100%' }}>
+            <Card bg="success" text="white" style={{ height: '150px' }}>
+              <Card.Body className="bp-0">
+                <Card.Title className="my-3 mx-5 h-1">
+                  <h2>알림</h2>
+                </Card.Title>
+                <Card.Title
+                  className="my-3 mx-5 h-1 bp-0"
+                  style={{ fontWeight: 'bold', height: '100px' }}
+                >
+                  <h2>마당</h2>
+                </Card.Title>
+              </Card.Body>
+            </Card>
+            <ButtonGroup vertical>
+              <Button
+                variant="outline-success"
+                className="mb-2 p-2 rounded"
+                size="lg"
+                block="true"
+              >
+                공지사항
+              </Button>
+              <Button
+                variant="outline-success"
+                className="mb-2 p-2 rounded"
+                size="lg"
+                block="true"
+              >
+                자주하는 질문
+              </Button>
+              <Button
+                variant="outline-success"
+                className="mb-2 p-2 rounded"
+                size="lg"
+                block="true"
+              >
+                1:1 고객센터
+              </Button>
+              {/* block button 세로 길이 조정 */}
+            </ButtonGroup>
+          </Col>
+          {/* // 3번 그리드 */}
+          <Col md={9}>
+            <Row>
+              <h2>공지사항</h2>
+              <hr />
+            </Row>
+            <Row className="mt-2">
+              <Col className="bg-light border pt-1">
+                <Col className="mb-1" style={{ float: 'right' }}>
+                  <Stack direction="horizontal" gap={3}>
+                    <Form.Control
+                      className="ms-auto"
+                      placeholder="Add your item here..."
+                    />
+                    <Button
+                      variant="success"
+                      text="white"
+                      style={{ width: '130px' }}
+                    >
+                      검색
+                    </Button>
+                  </Stack>
+                </Col>
+                <Col>
+                  {/* 서치바 드롭다운 메뉴 */}
+                  <Form.Select
+                    className="me-2"
+                    style={{ float: 'right', width: '100px' }}
+                  >
+                    <option>전체</option>
+                    <option value="1">최신순</option>
+                    <option value="2">게시글순</option>
+                    <option value="3">왓에버순</option>
+                  </Form.Select>
+                </Col>
+                <Col>
+                  {admin && (
+                    <Link to="/board/announce/add">
+                      <Button>글쓰기</Button>
+                    </Link>
+                  )}
+                </Col>
+              </Col>
+            </Row>
+            <Row>
+              {/* 게시물 기재 테이블 */}
+              <Table striped className="mt-4">
+                <thead>
+                  <tr>
+                    <th scope="col" width="10%">
+                      번호
+                    </th>
+                    <th scope="col" width="40%">
+                      제목
+                    </th>
+                    <th scope="col" width="15%">
+                      작성자
+                    </th>
+                    <th scope="col" width="20%">
+                      작성일
+                    </th>
+                    <th scope="col" width="10%">
+                      조회수
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {mainPosts.map((post, index) => (
+                    <AnnounceBoardList key={post.id} post={post} page={page} />
+                  ))}
+                </tbody>
+              </Table>
+            </Row>
+            <Row>
+              <Pagination
+                activePage={page}
+                itemsCountPerPage={10}
+                totalItemsCount={450}
+                pageRangeDisplayed={5}
+                prevPageText={'‹'}
+                nextPageText={'›'}
+                onChange={handlePageChange}
+              />
+            </Row>
+          </Col>
+        </Row>
+      </Container>
       <Footer>
         <div>
           <Footer_container>
--- front/src/components/UI/footer.jsx
+++ front/src/components/UI/footer.jsx
@@ -1,51 +1,76 @@
 import React from 'react';
-import { Container, Row, Col, Image
-,Dropdown, DropdownButton } from 'react-bootstrap';
-import footerlogo from '../../assets/footerlogo.png'
+import {
+  Container,
+  Row,
+  Col,
+  Image,
+  Dropdown,
+  DropdownButton,
+} from 'react-bootstrap';
+import footerlogo from '../../assets/footerlogo.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Footer() {
   return (
-    <footer className="bg-light py-3" >
+    <footer className="bg-light py-3">
       <Container>
         <Row>
           <Col sm={9}>
-            <Row className='mb-4 mt-2'>
-              <Col sm={3} style={{fontSize: "12px"}}>개인정보처리방침</Col>
-              <Col sm={3} style={{fontSize: "12px"}}>영상정보처리기기전리방침</Col>
-              <Col sm={3} style={{fontSize: "12px"}}>저작권 보호 </Col>
-              
+            <Row className="mb-4 mt-2">
+              <Col sm={3} style={{ fontSize: '12px' }}>
+                개인정보처리방침
+              </Col>
+              <Col sm={3} style={{ fontSize: '12px' }}>
+                영상정보처리기기전리방침
+              </Col>
+              <Col sm={3} style={{ fontSize: '12px' }}>
+                저작권 보호{' '}
+              </Col>
             </Row>
             <Row>
-            <hr style={{width: "60%"}}></hr>
+              <hr style={{ width: '60%' }}></hr>
             </Row>
             <Row>
-              <Col style={{fontSize: "12px"}}>
-              (12345)충청북도 제천시 세명로<br/>
-              COPYRIGHT Jecheon-do ALL Right reserved.
+              <Col style={{ fontSize: '12px' }}>
+                (12345)충청북도 제천시 세명로
+                <br />
+                COPYRIGHT Jecheon-do ALL Right reserved.
               </Col>
             </Row>
           </Col>
-          <Col sm={3} className="d-flex justify-content-end" >
+          <Col sm={3} className="d-flex justify-content-end">
             <Image
               className="mt-1"
               width={500}
               height={130}
               src={footerlogo}
-              alt="footerlogo" />
+              alt="footerlogo"
+            />
           </Col>
         </Row>
         <Row>
           <Col sm={3} style={{ backgroundColor: 'green' }}>
-            <p style={{ fontSize: '20px', color: "white", marginTop: "7px"}}><b style={{ fontSize: '30px' }}>러브투어</b>JECHEON</p>
-            </Col>
-            <Col sm={9} className="d-flex justify-content-end" style={{ backgroundColor: 'green' }}>
-            <DropdownButton className="me-5 pe-3"class="DropdownButton float-right" style={{marginTop: "14px"}} id="footerdropdown" variant="success" title="유관기관">
+            <p style={{ fontSize: '20px', color: 'white', marginTop: '7px' }}>
+              <b style={{ fontSize: '30px' }}>러브투어</b>JECHEON
+            </p>
+          </Col>
+          <Col
+            sm={9}
+            className="d-flex justify-content-end"
+            style={{ backgroundColor: 'green' }}
+          >
+            <DropdownButton
+              className="me-5 pe-3 DropdownButton float-right"
+              style={{ marginTop: '14px' }}
+              id="footerdropdown"
+              variant="success"
+              title="유관기관"
+            >
               <Dropdown.Item href="#/action-1">청풍호반 케이블카</Dropdown.Item>
               <Dropdown.Item href="#/action-2">렛츠 코레일</Dropdown.Item>
               <Dropdown.Item href="#/action-3">제천시의회</Dropdown.Item>
             </DropdownButton>
-            </Col>
+          </Col>
         </Row>
       </Container>
     </footer>

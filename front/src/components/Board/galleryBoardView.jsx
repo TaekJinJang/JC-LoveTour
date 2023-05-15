50427de3844dfe96b2af4a8e12dfbdf5997a3b82
siseo123 <tpdlf9801@gmail.com>
2023-05-16 02:36:08 +0900

    galleryBoardView 수정 및 카드모델 사용

--- front/src/components/Board/galleryBoardView.jsx
+++ front/src/components/Board/galleryBoardView.jsx
@@ -13,6 +13,15 @@ import {
   Nav, Navbar, NavDropdown
 } from 'react-bootstrap';
 
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn
+} from 'mdb-react-ui-kit';
+
 import GalleryBoardList from './galleryBoardList';
 import { useSelector } from 'react-redux';
 
@@ -26,20 +35,20 @@ function galleryBoardView() {
     <>
       <Container>
         {/* 상단 네비바 */}
-      <Row className='w-100% p-0'>
+        <Row className='w-100% p-0'>
           <Navbar bg="success" expand="lg" >
             <Container>
               <Navbar.Brand href="#home"><h4>홈</h4></Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav>
-                  <NavDropdown as="h5" title="알림마당" id="basic-nav-dropdown">
+                  <NavDropdown as="h5" title="제천 러브투어" id="basic-nav-dropdown">
                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                   </NavDropdown>
-                  <NavDropdown as="h5" title="공지사항" id="basic-nav-dropdown">
+                  <NavDropdown as="h5" title="사진 갤러리" id="basic-nav-dropdown">
                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
@@ -50,60 +59,94 @@ function galleryBoardView() {
             </Container>
           </Navbar>
         </Row>
-        
+
 
         {/* 사이드 메뉴 */}
         <Row className="mt-3 ps-1">
           <Col md={3} className="d-grid gap-2 ms" style={{ height: '100%' }} >
             <Card bg="success" text="white" style={{ height: '150px' }}>
               <Card.Body className='bp-0'>
-                <Card.Title className="my-3 mx-5 h-1" ><h2>알림</h2></Card.Title>
-                <Card.Title className="my-3 mx-5 h-1 bp-0" style={{ fontWeight: 'bold', height: '100px' }}><h2>마당</h2></Card.Title>
+                <Card.Title className="my-3 mx-5 h-1" ><h2>사진</h2></Card.Title>
+                <Card.Title className="my-3 mx-5 h-1 bp-0" style={{ fontWeight: 'bold', height: '100px' }}><h3>갤러리</h3></Card.Title>
               </Card.Body>
             </Card>
             <ButtonGroup vertical>
-              <Button variant="outline-success" className="mb-2 p-2 rounded" size="lg" block>공지사항</Button>
-              <Button variant="outline-success" className="mb-2 p-2 rounded" size="lg" block>자주하는 질문</Button>
-              <Button variant="outline-success" className="mb-2 p-2 rounded" size="lg" block>1:1 고객센터</Button>
+              <Button variant="outline-success" className="mb-2 p-2 rounded" size="lg" block>러브투어 소개</Button>
+              <Button variant="outline-success" className="mb-2 p-2 rounded" size="lg" block>지원 혜택</Button>
+              <Button variant="outline-success" className="mb-2 p-2 rounded" size="lg" block>사진 갤러리</Button>
+              <Button variant="outline-success" className="mb-2 p-2 rounded" size="lg" block>영상 갤러리</Button>
               {/* block button 세로 길이 조정 */}
             </ButtonGroup>
           </Col>
-        </Row>
-        <Row className="mt-4">
-          <Col md={3} className="d-grid gap-2">
 
-          </Col>
+
+
+          {/* 수정 진행 중 */}
           <Col md={9}>
             <Row>
-              <h2>Example heading</h2>
+              <h2>사진 갤러리</h2>
             </Row>
-            <Row>
-              <Col className="bg-light border">
-                <Form.Select>
+            <Row className="mt-2">
+              <Col className="bg-light border pt-1">
+                <Col className='mb-1' style={{ float: 'right' }}>
+                  <Stack direction="horizontal" gap={3}>
+                    <Form.Control className="ms-auto" placeholder="Add your item here..." />
+                    <Button variant="success" text="white" style={{ width: '130px' }}>검색</Button>
+
+                  </Stack>
+                </Col>
+                {/* 서치바 드롭다운 메뉴 통일 */}
+                <Form.Select className='me-2' style={{ float: 'right', width: '100px' }}>
                   <option>전체</option>
                   <option value="1">최신순</option>
                   <option value="2">게시글순</option>
                   <option value="3">왓에버순</option>
                 </Form.Select>
-                <Stack direction="horizontal" gap={3}>
-                  <Form.Control
-                    className="ms-auto"
-                    placeholder="Add your item here..."
-                  />
-                  <Button variant="success" text="white">
-                    검색
-                  </Button>
-                </Stack>
               </Col>
             </Row>
+
             <Row className="mt-4">
               <Col>
+                {/* 갤러리 예시 보이는 공간 */}
                 <Row>
+                  {/* 추가 하여 수정 필요 */}
+                  {/* 방법 구상 필요 */}
                   <Col md={4}>
-                    {gallery.map((post, index) => (
-                      <GalleryBoardList key={post.id} post={post} />
-                    ))}
+                    {/* 카드 모델의 일종 사용 */}
+                    <MDBCard>
+                      <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
+                      <MDBCardBody>
+                        <MDBCardTitle>1 title</MDBCardTitle>
+                        <MDBCardText>
+                          관광지 설명
+                        </MDBCardText>
+                      </MDBCardBody>
+                    </MDBCard>
                   </Col>
+                  <Col md={4}>
+                    <MDBCard>
+                      <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
+                      <MDBCardBody>
+                        <MDBCardTitle>2 title</MDBCardTitle>
+                        <MDBCardText>
+                          관광지 설명
+                        </MDBCardText>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </Col>
+                  <Col md={4}>
+                    <MDBCard>
+                      <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
+                      <MDBCardBody>
+                        <MDBCardTitle>3 title</MDBCardTitle>
+                        <MDBCardText>
+                          관광지 설명
+                        </MDBCardText>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </Col>
+
+
                 </Row>
               </Col>
             </Row>

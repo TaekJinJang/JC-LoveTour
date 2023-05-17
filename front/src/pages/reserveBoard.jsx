1f8494242850f83958e79da33fc7c034df2d3de8
suhyun <use07125@naver.com>
2023-05-18 01:11:08 +0900

    footer 추가

--- front/src/pages/reserveBoard.jsx
+++ front/src/pages/reserveBoard.jsx
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ReserveBoardView from '../components/Board/reserveBoardView';
 import Header from '../components/UI/header';
 // import { TextArea } from 'react-bootstrap';
+import Footer from '../components/UI/footer';
 
 function reserveBoard() {
   return (
     <>
       <Header />
       <ReserveBoardView />
+      <Footer />
     </>
   );
 }

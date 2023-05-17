210c31a9c49f8e3d7c5a46be6c81f2f029c31070
Taekjin Jang <xorwls1500@naver.com>
2023-05-17 23:53:54 +0900

    수상한 footer 삭제

--- front/src/components/Board/announceBoardWrite.jsx
+++ front/src/components/Board/announceBoardWrite.jsx
@@ -1,22 +1,37 @@
-import React, { useCallback, useRef, useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useCallback, useRef, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { useDispatch, useSelector } from "react-redux";
-import { ADD_POST_REQUEST, REMOVE_IMAGE, UPLOAD_IMAGES_REQUEST } from "../../reducers/post";
-import { Link, useNavigate } from "react-router-dom";
-import useInput from "../../hooks/useInput";
-import "../UI/paging.css";
-import "../UI/boardUI.css";
-import { Form, Button, Container, Row, Col, Card, Nav, Navbar, NavDropdown, Stack, ButtonGroup } from "react-bootstrap";
-import Footer from "../UI/footer";
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  ADD_POST_REQUEST,
+  REMOVE_IMAGE,
+  UPLOAD_IMAGES_REQUEST,
+} from '../../reducers/post';
+import { Link, useNavigate } from 'react-router-dom';
+import useInput from '../../hooks/useInput';
+import '../UI/paging.css';
+import '../UI/boardUI.css';
+import {
+  Form,
+  Button,
+  Container,
+  Row,
+  Col,
+  Card,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Stack,
+  ButtonGroup,
+} from 'react-bootstrap';
+import Footer from '../UI/footer';
 
 import Footer from '../UI/footer';
 
-
 function announceBoardWrite() {
   const { imagePaths } = useSelector((state) => state.post);
-  const [title, onChangeTitle] = useInput("");
-  const [text, onChangeText] = useInput("");
+  const [title, onChangeTitle] = useInput('');
+  const [text, onChangeText] = useInput('');
 
   const imageInput = useRef();
 
@@ -27,16 +42,16 @@ function announceBoardWrite() {
     (e) => {
       e.preventDefault();
       if (!text || !text.trim()) {
-        return alert("게시글을 작성하세요");
+        return alert('게시글을 작성하세요');
       }
       const formData = new FormData();
       imagePaths.forEach((p) => {
-        formData.append("image", p);
+        formData.append('image', p);
       });
-      formData.append("title", title);
-      formData.append("content", text);
+      formData.append('title', title);
+      formData.append('content', text);
       console.log(formData);
-      navigate("/board/announce");
+      navigate('/board/announce');
       return dispatch({
         type: ADD_POST_REQUEST,
         data: formData,
@@ -50,10 +65,10 @@ function announceBoardWrite() {
   }, [imageInput.current]);
 
   const onChangeImages = useCallback((e) => {
-    console.log("images", e.target.files);
+    console.log('images', e.target.files);
     const imageFormData = new FormData();
     [].forEach.call(e.target.files, (f) => {
-      imageFormData.append("image", f);
+      imageFormData.append('image', f);
     });
     dispatch({
       type: UPLOAD_IMAGES_REQUEST,
@@ -83,16 +98,32 @@ function announceBoardWrite() {
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav>
                   <NavDropdown as="h5" title="알림마당" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.1">
+                      Action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">
+                      Something
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.4">
+                      Separated link
+                    </NavDropdown.Item>
                   </NavDropdown>
                   <NavDropdown as="h5" title="공지사항" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.1">
+                      Action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">
+                      Something
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.4">
+                      Separated link
+                    </NavDropdown.Item>
                   </NavDropdown>
                 </Nav>
               </Navbar.Collapse>
@@ -102,25 +133,43 @@ function announceBoardWrite() {
 
         {/* // 2번 그리드 여기 아래의 코드가 바로 사이드바 코드 */}
         <Row className="mt-3 ps-1">
-          <Col md={3} className="d-grid gap-2 ms" style={{ height: "100%" }}>
-            <Card bg="success" text="white" style={{ height: "150px" }}>
+          <Col md={3} className="d-grid gap-2 ms" style={{ height: '100%' }}>
+            <Card bg="success" text="white" style={{ height: '150px' }}>
               <Card.Body className="bp-0">
                 <Card.Title className="my-3 mx-5 h-1">
                   <h2>알림</h2>
                 </Card.Title>
-                <Card.Title className="my-3 mx-5 h-1 bp-0" style={{ fontWeight: "bold", height: "100px" }}>
+                <Card.Title
+                  className="my-3 mx-5 h-1 bp-0"
+                  style={{ fontWeight: 'bold', height: '100px' }}
+                >
                   <h2>마당</h2>
                 </Card.Title>
               </Card.Body>
             </Card>
             <ButtonGroup vertical>
-              <Button variant="outline-success" className="mb-2 p-2 rounded" size="lg" block="true">
+              <Button
+                variant="outline-success"
+                className="mb-2 p-2 rounded"
+                size="lg"
+                block="true"
+              >
                 공지사항
               </Button>
-              <Button variant="outline-success" className="mb-2 p-2 rounded" size="lg" block="true">
+              <Button
+                variant="outline-success"
+                className="mb-2 p-2 rounded"
+                size="lg"
+                block="true"
+              >
                 자주하는 질문
               </Button>
-              <Button variant="outline-success" className="mb-2 p-2 rounded" size="lg" block="true">
+              <Button
+                variant="outline-success"
+                className="mb-2 p-2 rounded"
+                size="lg"
+                block="true"
+              >
                 1:1 고객센터
               </Button>
 
@@ -131,7 +180,7 @@ function announceBoardWrite() {
           <Col>
             <Col>
               <Row className="mb-4">
-                <h2 className="mb-4" style={{ color: "#2da57d" }}>
+                <h2 className="mb-4" style={{ color: '#2da57d' }}>
                   공지사항
                 </h2>
                 <hr />
@@ -141,14 +190,19 @@ function announceBoardWrite() {
               {/* 제목 */}
               <Form.Group as={Row} className="mb-3" controlId="title">
                 <Col md={2}>
-                  <Card className="text-center" bg="success" border="success" text="white">
+                  <Card
+                    className="text-center"
+                    bg="success"
+                    border="success"
+                    text="white"
+                  >
                     <Card.Header
                       style={{
-                        height: "35px",
-                        fontSize: "17px",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
+                        height: '35px',
+                        fontSize: '17px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
                       }}
                     >
                       제목
@@ -163,7 +217,7 @@ function announceBoardWrite() {
                     placeholder="제목을 입력해주세요. "
                     value={title}
                     onChange={onChangeTitle}
-                    style={{ backgroundColor: "#D9D9D9" }}
+                    style={{ backgroundColor: '#D9D9D9' }}
                   />
                 </Col>
               </Form.Group>
@@ -171,14 +225,19 @@ function announceBoardWrite() {
               {/* 내용 */}
               <Form.Group as={Row} className="mb-3" controlId="text">
                 <Col md={2}>
-                  <Card className="text-center" bg="success" border="success" text="white">
+                  <Card
+                    className="text-center"
+                    bg="success"
+                    border="success"
+                    text="white"
+                  >
                     <Card.Header
                       style={{
-                        height: "35px",
-                        fontSize: "17px",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
+                        height: '35px',
+                        fontSize: '17px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
                       }}
                     >
                       내용
@@ -195,7 +254,7 @@ function announceBoardWrite() {
                     placeholder="내용을 입력해주세요."
                     value={text}
                     onChange={onChangeText}
-                    style={{ backgroundColor: "#D9D9D9" }}
+                    style={{ backgroundColor: '#D9D9D9' }}
                   />
                 </Col>
               </Form.Group>
@@ -203,7 +262,11 @@ function announceBoardWrite() {
               {/* 이미지 업로드 */}
               <Form.Group as={Row} className="mb-3" controlId="text">
                 <Col md={2}>
-                  <Button onClick={onClickImageUpload} variant="success" style={{ width: "98px", fontSize: "17px" }}>
+                  <Button
+                    onClick={onClickImageUpload}
+                    variant="success"
+                    style={{ width: '98px', fontSize: '17px' }}
+                  >
                     이미지
                   </Button>
                 </Col>
@@ -215,14 +278,18 @@ function announceBoardWrite() {
                     ref={imageInput}
                     multiple
                     onChange={onChangeImages}
-                    style={{ backgroundColor: "#D9D9D9" }}
+                    style={{ backgroundColor: '#D9D9D9' }}
                   />
                 </Col>
               </Form.Group>
               <div>
                 {imagePaths.map((v, i) => (
-                  <div key={v} style={{ display: "inline-block" }}>
-                    <img src={`http://localhost:3005/${v}`} style={{ width: "200px" }} alt={v} />
+                  <div key={v} style={{ display: 'inline-block' }}>
+                    <img
+                      src={`http://localhost:3005/${v}`}
+                      style={{ width: '200px' }}
+                      alt={v}
+                    />
                     <div>
                       <Button onClick={onRemoveImage(i)}>제거</Button>
                     </div>
@@ -235,7 +302,7 @@ function announceBoardWrite() {
                   className="mb-4"
                   variant="success"
                   type="submit"
-                  style={{ width: "100px", borderRadius: "30px" }}
+                  style={{ width: '100px', borderRadius: '30px' }}
                 >
                   등록
                 </Button>
@@ -243,8 +310,6 @@ function announceBoardWrite() {
             </Form>
           </Col>
         </Row>
-        <Footer />
-
       </Container>
 
       <Footer />

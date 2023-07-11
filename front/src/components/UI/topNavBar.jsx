a398657bdc066223d32ec6f0c2fc89330b52d94b
Park-Dong-jun <seonmul4812@naver.com>
2023-07-11 20:18:50 +0900

    공백 해결후 탑 네비 추가

--- front/src/components/UI/topNavBar.jsx
+++ front/src/components/UI/topNavBar.jsx
@@ -19,15 +19,27 @@ function topNavBar() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav>
                         <NavDropdown as="h5" title="제천 러브투어" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">지원혜택</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">사진 갤러리</NavDropdown.Item>
+                            <NavDropdown.Item href="/board/supportBenefit">지원혜택</NavDropdown.Item>
+                            <NavDropdown.Item href="/board/gallery">사진 갤러리</NavDropdown.Item>
                         </NavDropdown>
                         <NavDropdown as="h5" title="제천의 이모저모" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">관광지</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">전통시장</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">음식</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.4">숙박</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.5">축제/행사</NavDropdown.Item>
+                            <NavDropdown.Item href="/board/touristSpot">관광지</NavDropdown.Item>
+                            <NavDropdown.Item href="/board/traditionalMarket">전통시장</NavDropdown.Item>
+                            <NavDropdown.Item href="/board/food">음식</NavDropdown.Item>
+                            <NavDropdown.Item href="/board/accommodation">숙박</NavDropdown.Item>
+                            <NavDropdown.Item href="/board/festival">축제/행사</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown as="h5" title="테마/코스" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/board/oksunbongPeakCourse">옥순봉 코스</NavDropdown.Item>
+                            <NavDropdown.Item href="/board/cheongpunghoCourse">청풍호 코스</NavDropdown.Item>
+                            <NavDropdown.Item href="/board/museumCourse">박물관 코스</NavDropdown.Item>
+                            <NavDropdown.Item href="/board/shrineOfBaeronCourse">배론성지 코스</NavDropdown.Item>
+                            <NavDropdown.Item href="/board/healingCourse">힐링 코스</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown as="h5" title="알림사항" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/board/announce">공지사항</NavDropdown.Item>
+                            <NavDropdown.Item href="/board/faq">자주하는 질문</NavDropdown.Item>
+                            <NavDropdown.Item href="/board/review">투어 후기</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>

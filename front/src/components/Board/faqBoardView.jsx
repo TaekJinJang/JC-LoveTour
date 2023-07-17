5fd63e3c19f82c5acdf5713e41ea2330f313249d
siseo123 <tpdlf9801@gmail.com>
2023-07-17 17:12:47 +0900

    알림마당 네비바 수정 리뷰페이지 제외 (사유 : 작업중인 페이지)

--- front/src/components/Board/faqBoardView.jsx
+++ front/src/components/Board/faqBoardView.jsx
@@ -1,9 +1,9 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link, useNavigate } from 'react-router-dom';
-import Pagination from 'react-js-pagination';
-import '../UI/paging.css';
-import styled from 'styled-components';
+import React, { useCallback, useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link, useNavigate } from "react-router-dom";
+import Pagination from "react-js-pagination";
+import "../UI/paging.css";
+import styled from "styled-components";
 import {
   ButtonGroup,
   Button,
@@ -13,23 +13,24 @@ import {
   Nav,
   Navbar,
   NavDropdown,
-} from 'react-bootstrap';
+} from "react-bootstrap";
 
 // import GalleryBoardList from './galleryBoardList';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 
-import { LOAD_GALLERY_POSTS_REQUEST } from '../../reducers/post';
+import { LOAD_GALLERY_POSTS_REQUEST } from "../../reducers/post";
 
 // 공통부분
-import { Container, Row, Col } from 'react-bootstrap';
-import Header from '../UI/header';
-import TopNavBar from '../UI/topNavBar';
-import SideBar from '../UI/sideBar';
-import Footer from '../UI/footer';
+import { Container, Row, Col } from "react-bootstrap";
+import Header from "../UI/header";
+import TopNavBar from "../UI/topNavBar";
+import SideBar from "../UI/sideBar";
+import Footer from "../UI/footer";
+import MobileHeaders from "../UI/mobileHeaders";
 
 function faqBoardView() {
   // 페이지 버튼 눌린 상태로 만드려고 생성
-  const [currentPage, setCurrentPage] = useState('FAQ'); // 현재 페이지 상태
+  const [currentPage, setCurrentPage] = useState("FAQ"); // 현재 페이지 상태
   const { admin } = useSelector((state) => state.admin);
   const { gallery } = useSelector((state) => state.post);
   const dispatch = useDispatch();
@@ -54,26 +55,28 @@ function faqBoardView() {
 
   // 사이드바 내용
   const buttons = [
-    { label: '공지사항', href: '/board/announce' },
-    { label: '자주하는 질문', href: '/board/faq' },
-    { label: '투어 후기', href: '/board/review' },
+    { label: "공지사항", href: "/board/announce" },
+    { label: "자주하는 질문", href: "/board/faq" },
+    { label: "투어 후기", href: "/board/review" },
   ];
 
   return (
     <>
-      <Container style={{fontFamily: 'Pretendard-Regular',}}>
-        <Header />
+      <Container style={{ fontFamily: "Pretendard-Regular" }}>
+        <MobileHeaders />
+        {/* <Header /> */}
         <Container>
-          <Row style={{ width: '100%', marginLeft: 0, marginRight: 0 }}>
+          {/* <Row style={{ width: '100%', marginLeft: 0, marginRight: 0 }}>
             <TopNavBar />
-          </Row>
-          <Row className="mt-3 ps-1" style={{ width: '100%' }}>
-            <Col md={3}>
-              <SideBar buttons={buttons} title={'알림마당'} />
+          </Row> */}
+          <Row className="mt-3 ms-0" style={{ width: "100%" }}>
+            <Col md={3} className="ps-0 pe-1">
+              <SideBar buttons={buttons} title={"알림마당"} />
             </Col>
-            <Col md={9}>
-              <Row>
+            <Col md={9} className="ps-0">
+              <Row className="ms-2">
                 <h3>자주하는 질문</h3>
+                <hr />
               </Row>
             </Col>
           </Row>

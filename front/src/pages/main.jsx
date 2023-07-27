485bc95b39345b9dd0b5c8e46be9864659ee8039
Taek_Jini <xorwls1500@naver.com>
2023-07-27 10:04:54 +0900

    fix :: import name Fix

--- front/src/pages/main.jsx
+++ front/src/pages/main.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 import Footer from "../components/UI/footer";
 
-import MainHeader from "../components/UI/mainHeader";
+import MainHeader from "../components/UI/mainheader";
 
 import MainGrid from "../components/UI/mainGrid";
 

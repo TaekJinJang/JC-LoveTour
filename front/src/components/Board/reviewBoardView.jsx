d39ec5f9c7e39376035f1417af31b248966a5b72
Park-Dong-jun <seonmul4812@naver.com>
2023-09-07 13:08:01 +0900

    리뷰게시판 수정

--- front/src/components/Board/reviewBoardView.jsx
+++ front/src/components/Board/reviewBoardView.jsx
@@ -63,7 +63,7 @@ function reviewBoardView() {
             {/* 상단이미지 */}
             <Container
                 fluid
-                style={{ height: '40vh', width: '98vw', overflowX: 'hidden' }}
+                style={{ height: '40vh', width: '100vw', overflowX: 'hidden' }}
                 className="container-fluid m-0 p-0"
             >
                 <div
@@ -82,108 +82,109 @@ function reviewBoardView() {
                     <h1 style={{ color: 'white' }}>알림마당</h1>
                 </div>
             </Container>
+            <Container fluid="sm" className="mt-5">
+                <Row className="mt-3 ps-1" style={{ width: '100%' }}>
+                    <Col md={3}>
+                        <SideBar buttons={buttons} title={'알림마당'} />
+                    </Col>
+                    <Col md={9}>
+                        <Row>
+                            <h3>투어후기</h3>
+                            <hr />
+                        </Row>
+                        <Row className="mt-2">
+                            <Col className="bg-light border pt-1">
+                                <Col className="mb-1" style={{ float: 'right' }}>
+                                    <Stack direction="horizontal" gap={3}>
+                                        <Form.Control type="text" value={searchInput} onChange={onChangeSearchInput} />
+                                        <Button
+                                            variant="success"
+                                            text="white"
+                                            style={{ width: '130px' }}
+                                            onClick={goToSearch}
+                                        >
+                                            검색
+                                        </Button>
+                                    </Stack>
+                                </Col>
+                                <Col>
+                                    {/* 서치바 드롭다운 메뉴 */}
 
-            <Row className="mt-3 ps-1" style={{ width: '100%' }}>
-                <Col md={3}>
-                    <SideBar buttons={buttons} title={'알림마당'} />
-                </Col>
-                <Col md={9}>
-                    <Row>
-                        <h3>투어후기</h3>
-                        <hr />
-                    </Row>
-                    <Row className="mt-2">
-                        <Col className="bg-light border pt-1">
-                            <Col className="mb-1" style={{ float: 'right' }}>
-                                <Stack direction="horizontal" gap={3}>
-                                    <Form.Control type="text" value={searchInput} onChange={onChangeSearchInput} />
-                                    <Button
-                                        variant="success"
-                                        text="white"
-                                        style={{ width: '130px' }}
-                                        onClick={goToSearch}
-                                    >
-                                        검색
-                                    </Button>
-                                </Stack>
+                                    <Form.Select className="me-3" style={{ float: 'right', width: '100px' }}>
+                                        <option>전체</option>
+                                        <option value="1">최신순</option>
+                                        <option value="2">게시글순</option>
+                                        <option value="3">왓에버순</option>
+                                    </Form.Select>
+                                </Col>
+                                <Col>
+                                    <Link to="/board/review/add">
+                                        <Button variant="success">후기쓰기</Button>
+                                    </Link>
+                                </Col>
                             </Col>
-                            <Col>
-                                {/* 서치바 드롭다운 메뉴 */}
-
-                                <Form.Select className="me-3" style={{ float: 'right', width: '100px' }}>
-                                    <option>전체</option>
-                                    <option value="1">최신순</option>
-                                    <option value="2">게시글순</option>
-                                    <option value="3">왓에버순</option>
-                                </Form.Select>
-                            </Col>
-                            <Col>
-                                <Link to="/board/review/add">
-                                    <Button variant="success">후기쓰기</Button>
-                                </Link>
-                            </Col>
-                        </Col>
-                    </Row>
-                    <Row>
-                        {/* 게시물 기재 테이블 */}
-                        <Table bordered className="mt-4 table table-hover" style={{ border: '1px solid #f2f2f2' }}>
-                            <thead
-                                style={{
-                                    textAlign: 'center',
-                                    backgroundColor: '#E0E0E0',
-                                }}
-                            >
-                                <tr>
-                                    <th scope="col" width="20%">
-                                        글번호
-                                    </th>
-                                    <th scope="col" width="20%">
-                                        작성자
-                                    </th>
-                                    <th scope="col" width="20%">
-                                        작성일
-                                    </th>
-                                    {/* <th scope="col" width="20%">
+                        </Row>
+                        <Row>
+                            {/* 게시물 기재 테이블 */}
+                            <Table bordered className="mt-4 table table-hover" style={{ border: '1px solid #f2f2f2' }}>
+                                <thead
+                                    style={{
+                                        textAlign: 'center',
+                                        backgroundColor: '#E0E0E0',
+                                    }}
+                                >
+                                    <tr>
+                                        <th scope="col" width="20%">
+                                            글번호
+                                        </th>
+                                        <th scope="col" width="20%">
+                                            작성자
+                                        </th>
+                                        <th scope="col" width="20%">
+                                            작성일
+                                        </th>
+                                        {/* <th scope="col" width="20%">
                         작성일
                       </th> */}
-                                    <th scope="col" width="40%">
-                                        내용
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody
-                                style={{
-                                    textAlign: 'center',
-                                }}
-                            >
-                                {currentPosts.map((post, index) => (
-                                    <ReviewBoardList
-                                        key={post.id}
-                                        post={post}
-                                        //   page={page}
-                                    />
-                                ))}
-                            </tbody>
-                        </Table>
-                    </Row>
-                    <Row>
-                        {/* <Col className="mt-3 ps-0 pe-0 " style={{ width: '100%' }}>
+                                        <th scope="col" width="40%">
+                                            내용
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody
+                                    style={{
+                                        textAlign: 'center',
+                                    }}
+                                >
+                                    {currentPosts.map((post, index) => (
+                                        <ReviewBoardList
+                                            key={post.id}
+                                            post={post}
+                                            //   page={page}
+                                        />
+                                    ))}
+                                </tbody>
+                            </Table>
+                        </Row>
+                        <Row>
+                            {/* <Col className="mt-3 ps-0 pe-0 " style={{ width: '100%' }}>
                                     {' '}
                                     {currentPosts.map((post, index) => (
                                         <ReviewBoardList key={post.id} post={post} />
                                     ))} */}
-                        <Pagination
-                            activePage={page}
-                            itemsCountPerPage={10}
-                            totalItemsCount={reviewPosts.length}
-                            pageRangeDisplayed={5}
-                            prevPageText={'‹'}
-                            nextPageText={'›'}
-                            onChange={handlePageChange}
-                        />
-                    </Row>
-                </Col>
-            </Row>
+                            <Pagination
+                                activePage={page}
+                                itemsCountPerPage={10}
+                                totalItemsCount={reviewPosts.length}
+                                pageRangeDisplayed={5}
+                                prevPageText={'‹'}
+                                nextPageText={'›'}
+                                onChange={handlePageChange}
+                            />
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
 
             <Footer />
         </>
